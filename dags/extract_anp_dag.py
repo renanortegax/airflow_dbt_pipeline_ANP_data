765789ceeb91056f1d104269f979b47a97d41452
renanortegax <renan.xavier44@gmail.com>
2026-01-29 00:06:38 -0300

    Feat: inclui na DAG novas tasks de carga dos dados no postgres

--- dags/extract_anp_dag.py
+++ dags/extract_anp_dag.py
@@ -5,16 +5,14 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
 # from airflow.providers.postgres.operators.postgres import PostgresOperator
 # from airflow.providers.postgres.hooks.postgres import PostgresHook
-from include.extract_anp import extract_anp_data
+from include.extract_anp_raw import extract_anp_data
+from include.load_anp_into_postgres import create_table_if_not_exists, load_dataset_into_db
+from include.utils import dataset_names
 
-
-DATA_SET = {
-    'diesel_gnv':'ultimas-4-semanas-diesel-gnv.csv',
-    'gasolina_etano':'ultimas-4-semanas-gasolina-etanol.csv',
-    'glp':'ultimas-4-semanas-glp.csv'
-}
+DATA_SET = dataset_names()
 
 url_standard = 'www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/qus'
+output_raw_path = "/usr/local/airflow/data/raw/anp"
 
 with DAG(
     dag_id='extract_data_anp',
@@ -27,20 +25,46 @@ with DAG(
     catchup=False, # faz retroativo
     tags=['anp']
 ) as dag:
-    start = EmptyOperator(
-        task_id='starting'
-    )
+    
+    start = EmptyOperator(task_id='starting')
+    end = EmptyOperator(task_id='end')
 
     extract_anp_task = PythonOperator(
         task_id="extract_anp_raw_data",
         python_callable=extract_anp_data,
         op_kwargs={
-            "output_dir": "/usr/local/airflow/data/raw/anp"
+            "output_dir": output_raw_path
         }
     )
 
-    end = EmptyOperator(
-        task_id='end'
-    )
+    # primeira parte da dag antes de trifurcar:
+    start >> extract_anp_task
 
-    start >> extract_anp_task >> end
+    for dataset in DATA_SET.keys():
+        create_table_task = PythonOperator(
+            task_id=f"create_table_{dataset}",
+            python_callable=create_table_if_not_exists,
+            op_kwargs={
+                "dataset_name":dataset
+            }
+        )
+
+        load_data_task = PythonOperator(
+            task_id=f"load_into_db_{dataset}",
+            python_callable=load_dataset_into_db,
+            op_kwargs={
+                "dataset_name":dataset,
+                "parquet_path":f"{output_raw_path}/{dataset}.parquet"
+            }
+        )
+
+        # segunda parte (uma pra cada dataset)
+        extract_anp_task >> create_table_task >> load_data_task >> end
+
+
+
+#                 ┌─ create_table_gasolina ─ load_gasolina ─┐
+#                │                                          │
+# start → extract├─ create_table_diesel   ─ load_diesel   ──┼→ end
+#                │                                          │
+#                 └─ create_table_glp      ─ load_glp      ─┘
