43bd37e531697d5888805867fa5cc033f29c5510
renanortegax <renan.xavier44@gmail.com>
2026-01-29 00:07:07 -0300

    Add: arquivo sql para validar insercao dos dados

---
+++ postgres_astro.session.sql
@@ -0,0 +1,17 @@
+SELECT
+table_schema
+,table_name
+FROM information_schema.tables
+WHERE 1=1
+    and table_schema NOT IN ('pg_catalog', 'information_schema')
+    -- AND table_type = 'BASE TABLE';;
+
+
+select *
+from raw_diesel_gnv
+limit 100;
+
+
+drop table raw_diesel_gnv;
+drop table raw_gasolina_etanol;
+drop table raw_glp;
